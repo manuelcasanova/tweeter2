1a096146f1d2722ec0ea2e6ff058a505db9cc996
Manuel Casanova <manucasanova@hotmail.com>
2022-03-17 16:22:24 -0700

    Implemented function to change the count. Implemented if statement to make it red and negative after 140 characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,27 @@
 // $(document).ready(function() {
-//   $("textarea").on("input", function(){
-//     console.log("Hello");
-//   });
-// });
-
-// $(document).ready(function() {
-//   $("textarea").on("input", function(){
-//     console.log(this);
-//   });
-// });
-
-$(document).ready(function() {
-   $('textarea').on('input', function() {
-     let tweetLength = $(this).val().length; //Prints to the console a number, the length of the input in the text area
+//    $('textarea').on('input', function() {
+//      let tweetLength = $(this).val().length; //Prints to the console a number, the length of the input in the text area
  
-  console.log(tweetLength);
+//   console.log(tweetLength);
 
-   });
+//    });
  
- });
+//  });
+
+$(document).ready(function () {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function (event) {
+    const maxLength = 140;
+    let tweet = event.target.value;
+    let remCharacters = maxLength - tweet.length;
+    //console.log(tweet);
+    $(".counter").text(remCharacters);
+    if (remCharacters < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black");
+    }
+  });
+});
+
+
