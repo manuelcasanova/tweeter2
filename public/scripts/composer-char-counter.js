f39f8a261f2ce0950fc12da9be1abff893abcfed
Manuel Casanova <manucasanova@hotmail.com>
2022-03-17 14:58:31 -0700

    Selected input out of the functions suggested in compass to print in the console the length of the input in the textarea box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,21 @@
+// $(document).ready(function() {
+//   $("textarea").on("input", function(){
+//     console.log("Hello");
+//   });
+// });
+
+// $(document).ready(function() {
+//   $("textarea").on("input", function(){
+//     console.log(this);
+//   });
+// });
+
 $(document).ready(function() {
-  $("textarea").on("input", function(){
-    console.log("Hello");
-  });
-});
+   $('textarea').on('input', function() {
+     let tweetLength = $(this).val().length; //Prints to the console a number, the length of the input in the text area
+ 
+  console.log(tweetLength);
+
+   });
+ 
+ });
